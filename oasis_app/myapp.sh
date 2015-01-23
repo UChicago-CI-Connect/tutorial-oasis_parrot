a2b2af6c010a26f3b27e2fd27b3f3256d69e843d
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-01-23 15:11:00 -0600

    Use OASIS instead of PALMS

--- oasis_app/myapp.sh
+++ oasis_app/myapp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /cvmfs/oasis.opensciencegrid.org/osg/palms/setup
-palmsdosetup R
+source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
+module load R
 Rscript ./oasis_app/test.R
 echo "Finishing script at: "
 echo `date`
